91466a5433de6517ea19edc84a0fbffc30e9c2d4
marcelo.c.silva <marcelo.c.silva@EISA-NB160.eisa.corp.com>
2017-05-16 11:49:56 -0300

    Adicionando o requestMapping

--- SpringForumConceito/src/main/java/com/marcelo/controladores/HomeController.java
+++ SpringForumConceito/src/main/java/com/marcelo/controladores/HomeController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
@@ -13,7 +14,7 @@ public class HomeController {
 	@Autowired
 	private DAOAssunto daoAssunto;
 	
-
+   @RequestMapping("/")
 	public String index(Map<String, Object> model){
 		model.put("assuntos", getDaoAssunto().list());
 		model.put("usuarios", getDaoUsuario().list());
