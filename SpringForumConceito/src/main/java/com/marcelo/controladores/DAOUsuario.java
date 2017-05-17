ff0c5795329985c740dbac3feb1ecfe4a530d16d
marcelo.c.silva <marcelo.c.silva@EISA-NB160.eisa.corp.com>
2017-05-17 11:10:01 -0300

    adicionando validações de campos

--- SpringForumConceito/src/main/java/com/marcelo/controladores/DAOUsuario.java
+++ SpringForumConceito/src/main/java/com/marcelo/controladores/DAOUsuario.java
@@ -1,6 +1,59 @@
 package com.marcelo.controladores;
 
+import java.util.Date;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
 public class DAOUsuario {
+	
+	@Size(max=128) @NotNull @NotEmpty
+	private String nome;
+	
+	@Email
+	private String email;
+	
+	@NotNull
+	private Date dataCadastro = new Date();
+	
+	@NotNull @NotEmpty
+	@Size(min=8, max=32, message="Login muito curto ou muito longo")
+	private String login;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
 
 	public Object list() {
 		// TODO Auto-generated method stub
--- SpringForumConceito/src/main/java/com/marcelo/controladores/HomeController.java
+++ SpringForumConceito/src/main/java/com/marcelo/controladores/HomeController.java
@@ -1,24 +1,51 @@
 package com.marcelo.controladores;
 
+import java.util.HashMap;
 import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 	@Autowired
 	private DAOUsuario daoUsuario;
-	
+
 	@Autowired
 	private DAOAssunto daoAssunto;
-	
-   @RequestMapping("/")
-	public String index(Map<String, Object> model){
+
+	@RequestMapping("/")
+	public String index(Map<String, Object> model) {
 		model.put("assuntos", getDaoAssunto().list());
 		model.put("usuarios", getDaoUsuario().list());
 		return "index";
+	}
+
+	@RequestMapping("/registro")
+	public String registro(Map<String, Object> model) {
+		if (model.get("usuario") == null) {
+			model.put("usuario", new Usuario());
+		}
+		return "registro";
+	}
+
+	@RequestMapping(value = "/executarRegistro", method = RequestMethod.POST)
+	public String executarRegistro(@Valid Usuario usuario, BindingResult bindingResult, HttpSession sessao) {
+		if (bindingResult.hasErrors()) {
+			Map<String, Object> model = new HashMap<String, Object>();
+			model.put("usuario", usuario);
+			return registro(model);
+		}
+
+		getDaoUsuario().persistir(usuario);
+		sessao.setAttribute("usuario", usuario);
+		return "redirect:/";
 	}
 
 	public DAOUsuario getDaoUsuario() {
